3f70d9c8b35ca419cc0452f1ea19aaddd9c5092b
viharadk <dv8268927@gmail.com>
2026-03-08 17:32:14 +0530

    Installed router and added PostTrip component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import PostTrip from './PostTrip';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={{ background: '#007bff', color: 'white', padding: '10px' }}>
+        <h1>Tuk Ride Share - Student Project</h1>
       </header>
+      
+      <main style={{ padding: '20px' }}>
+        <AboutUs />
+        <hr />
+        <ContactUs />
+        <hr />
+        <PostTrip />
+      </main>
     </div>
   );
 }
