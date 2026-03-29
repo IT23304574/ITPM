3f808c55b99375a14a039e976559fb86677f566d
amasha-sinhalagoda <amashavs03@gmail.com>
2026-03-29 11:29:36 +0530

    add recharge routes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const connectDB = require('./db');
 const authRoutes = require('./routes/authRoutes');
 const tripRoutes = require('./routes/tripRoutes');
 const requestRoutes = require('./routes/requestRoutes');
-
+const rechargeRoutes = require("./routes/rechargeRoutes");
 const app = express();
 
 // Middleware
@@ -20,8 +20,8 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/trips', tripRoutes);
 app.use('/api/requests', requestRoutes);
-
-const PORT = process.env.PORT || 5000;
+app.use("/api/recharge", rechargeRoutes);
+const PORT = process.env.PORT || 5001;
 
 const start = async () => {
   try {
